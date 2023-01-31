933843d8788eb6054723397fb9eff4e9c70cd6f1
Keyan <keyantabor98@gmail.com>
2023-01-30 21:27:56 -0500

    complete cc 31

--- python/code_challenges/hashtable_repeated_word.py
+++ python/code_challenges/hashtable_repeated_word.py
@@ -1,5 +1,12 @@
 from data_structures.hashtable import Hashtable
+import re 
 
-
-def first_repeated_word():
-    pass
+def first_repeated_word(string):
+    words = re.findall(r'\w+', string)
+    hashtable = Hashtable()
+    for word in words:
+        if hashtable.has(word):
+            return word
+        else:
+            hashtable.set(word, word)
+    return None
